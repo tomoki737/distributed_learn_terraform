efa17cdccc50601ba73f05bf393c5bd82ba7ab44
tomoki737 <tomoki.k2159@gmail.com>
2022-02-23 21:10:10 +0900

    ルートテーブルの追加

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_1a" {
   vpc_id = aws_vpc.main.id
-
+  map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
 
   cidr_block = "10.0.1.0/24"
@@ -33,6 +33,26 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "book_question_private_igw"
+    Name = "book_question_igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "book_question_private_public"
+  }
+}
+
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
