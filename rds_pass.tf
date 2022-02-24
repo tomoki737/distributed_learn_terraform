df603148cb2bf21ba32ef04b2574c5a6bed1173c
tomoki737 <tomoki.k2159@gmail.com>
2022-02-24 14:16:17 +0900

    gitignore修正

--- rds_pass.tf
+++
@@ -1,9 +0,0 @@
-
-variable "db_username" {
-    type        = string
-    default     = "tomoki"
-}
-variable "db_password" {
-    type        = string
-    default     = "ijoidjf[jofjd]**7783jnlvdm"
-}
